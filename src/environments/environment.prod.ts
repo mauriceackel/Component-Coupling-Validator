716e58dd7dedee0de03f2f3b0ec2756bba0c94af
Maurice <m.ackel@icloud.com>
2020-04-09 14:58:38 +0200

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzYN7Id-HtrJxZt6SNGUwDJ11gLDOlFDg",
+    authDomain: "integrateit-41c60.firebaseapp.com",
+    databaseURL: "https://integrateit-41c60.firebaseio.com",
+    projectId: "integrateit-41c60",
+    storageBucket: "integrateit-41c60.appspot.com",
+    messagingSenderId: "287628349300",
+    appId: "1:287628349300:web:70bba41d86cba288446621"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAzYN7Id-HtrJxZt6SNGUwDJ11gLDOlFDg",
+    authDomain: "integrateit-41c60.firebaseapp.com",
+    databaseURL: "https://integrateit-41c60.firebaseio.com",
+    projectId: "integrateit-41c60",
+    storageBucket: "integrateit-41c60.appspot.com",
+    messagingSenderId: "287628349300",
+    appId: "1:287628349300:web:70bba41d86cba288446621"
+  }
 };
 
 /*
