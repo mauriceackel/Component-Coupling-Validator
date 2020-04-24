4e8e89f391cec28ed2725c8f8c1be92ead50f2c2
Maurice <m.ackel@icloud.com>
2020-04-24 16:30:01 +0200

    Added possibility to disable editor zone and json editor

--- src/app/components/editor-zone/editor-zone.component.ts
+++ src/app/components/editor-zone/editor-zone.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-editor-zone',
   templateUrl: './editor-zone.component.html',
 })
 export class EditorZoneComponent implements OnInit, OnChanges {
+
+  @Input("disabled") public disabled: boolean;
 
   @Input("leftHeading") public leftHeading: string;
   @Input("rightHeading") public rightHeading: string;
@@ -21,11 +23,15 @@ export class EditorZoneComponent implements OnInit, OnChanges {
   constructor() { }
 
   public ngOnInit() {
-    this.uncommittedLeftData = this.leftData;
-    this.uncommittedRightData = this.rightData;
   }
 
-  public ngOnChanges() {
+  public ngOnChanges(changes: SimpleChanges) {
+    if (changes.leftData) {
+      this.uncommittedLeftData = this.leftData;
+    }
+    if (changes.rightData) {
+      this.uncommittedRightData = this.rightData;
+    }
     this.ngOnInit();
   }
 
