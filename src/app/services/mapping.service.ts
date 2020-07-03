5c48ff04296297f4b4ebd052dfa168f4421e665e
Maurice <m.ackel@icloud.com>
2020-07-03 16:38:37 +0200

    Fixed a bug that did not account for quoted strings inside a JSONata mapping

--- src/app/services/mapping.service.ts
+++ src/app/services/mapping.service.ts
@@ -251,6 +251,6 @@ export enum MappingDirection {
    * { "foo":"bar" } --> { "foo":bar }
    */
 export function stringifyedToJsonata(obj: string) {
-  const keyValueRegex = /(?:\"|\')([^"]*)(?:\"|\')(?=:)(?:\:\s*)(?:\"|\')?(true|false|[^"]*)(?:"(?=\s*(,|})))/g;
-  return obj.replace(keyValueRegex, '"$1":$2');
+  const keyValueRegex = /(?:\"|\')([^"]*)(?:\"|\')(?=:)(?:\:\s*)(?:\"|\')?(true|false|(?:[^"]|\\\")*)(?:"(?=\s*(,|})))/g;
+  return obj.replace(keyValueRegex, '"$1":$2').replace(/\\\"/g, '"');
 }
