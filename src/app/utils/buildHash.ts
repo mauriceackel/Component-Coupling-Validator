54a0a58eddffa81aecb0f431c6cf0546bf038c83
Maurice <m.ackel@icloud.com>
2020-07-28 16:54:45 +0200

    Added utility to build sha1 hash

---
+++ src/app/utils/buildHash.ts
@@ -0,0 +1,9 @@
+declare var window: { crypto: any, msCrypto: any };
+
+const crypto = window.crypto || window.msCrypto;
+
+export default async function createSha1Hash(message): Promise<string> {
+  const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(message));
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
